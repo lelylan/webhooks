c7c3132c9198ecd227159df11b43e165e1243684
Andrea Reginato <andrea.reginato@gmail.com>
2012-10-30 13:57:46 +0100

    Added logic in a specific module and fixed some typos on factories definition

--- app/models/jobs/event.js
+++ app/models/jobs/event.js
@@ -2,6 +2,7 @@ var mongoose = require('mongoose')
   , db = mongoose.createConnection('localhost', 'jobs_test');
 
 var eventSchema = new mongoose.Schema({
+    resource_owner_id: mongoose.Schema.Types.ObjectId,
     resource: String,
     event: String,
     body: mongoose.Schema.Types.Mixed
--- spec/app.spec.js
+++ spec/app.spec.js
@@ -1,6 +1,6 @@
-var nock    = require('nock')
-  , fs      = require('fs')
-  , request = require('request');
+var nock  = require('nock')
+  , fs    = require('fs')
+  , logic = require('../lib/logic');
 
 var Event        = require('../app/models/jobs/event')
   , Subscription = require('../app/models/subscriptions/subscription')
@@ -10,29 +10,27 @@ var Event        = require('../app/models/jobs/event')
 
 describe('when a new event happens', function() {
 
-  var user, app, token, event, sub, callback;
+  var user, application, token, event, sub, callback;
   var fixture = __dirname + '/fixtures/event.json';
 
   // Open the stream to raise the HTTP request
-  Event.find().tailable().stream().on('data', function (doc) {
-    request('http://www.google.com/', function (error, response, body) {})
-  });
+  Event.find().tailable().stream().on('data', function (doc) { logic.execute(); });
 
-  beforeEach(function() {
-    callback = nock('http://www.google.com').get('/').reply(200);
-  });
+  // Mock the HTTP request
+  beforeEach(function() { callback = nock('http://www.google.com').get('/').reply(200); });
 
+  // Create the needed data
   beforeEach(function(){
-    user  = User.create({ });
-    app   = Application.create({ });
-    token = AccessToken.create({ resource_owner_id: user._id, application: app._id, expires_in: 7200 })
-    event = Event.create({ resource: 'status', event: 'update', body: {} }, function (err, doc) {});
-    subs  = Subscription.create({ client_id: app._id, resource: 'status', event: 'update', callback: 'http://www.google.com'} );
+    User.create({ }, function (err, doc) { user = doc; });
+    Application.create({ }, function (err, doc) { application = doc} );
+    setTimeout(function() { AccessToken.create({ resource_owner_id: user._id, application: application._id, expires_in: 7200 }, function (err, doc) { token = doc }) }, 100);
+    setTimeout(function() { Subscription.create({ client_id: application._id, resource: 'status', event: 'update', callback: 'http://www.google.com'}, function (err, doc) { subscription = doc }); }, 100);
+    setTimeout(function() { Event.create({ resource_owner_id: user._id, resource: 'status', event: 'update', body: {} }, function (err, doc) { event = doc }); }, 100)
+    //setTimeout(function() { console.log(user)}, 500);
   });
 
-  it('fires a callback', function(done) {
+  it('makes an HTTP request to the subscription URI callback', function(done) {
     // will throw an assertion error if meanwhile a request was not performed
-    setTimeout(function() { callback.done(); done(); }, 100);
+    setTimeout(function() { callback.done(); done(); }, 200);
   });
-
 });
